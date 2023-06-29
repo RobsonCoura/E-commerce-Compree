b9fb0f5a581cd910b723063f75f152c75aad10e7
Robson Coura <robson_coura@yahoo.com.br>
2023-06-28 23:51:34 -0300

    Criando providers em UsuarioModule para gerenciar objetos

--- loja/src/usuario/usuario.module.ts
+++ loja/src/usuario/usuario.module.ts
@@ -1,7 +1,10 @@
 import { Module } from "@nestjs/common";
 import { UsuarioController } from "./usuario.controller";
+import { UsuarioRepository } from "./usuario.repository";
 
 @Module({
-    controllers: [UsuarioController]
+    controllers: [UsuarioController],
+    //Referencias para Classe que o Nest.js gerencie a criacao desses objetos.
+    providers: [UsuarioRepository]
 })
 export class UsuarioModule {}
