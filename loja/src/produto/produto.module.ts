2d98b8e2c5dd34d0436d6135886a50172c789183
Robson Coura <robson_coura@yahoo.com.br>
2023-06-29 00:30:11 -0300

    Criando providers em ProdutoModule para gerenciar objetos

--- loja/src/produto/produto.module.ts
+++ loja/src/produto/produto.module.ts
@@ -1,8 +1,12 @@
 import { Module } from "@nestjs/common";
+import { ProdutoRepository } from "./produto.repository";
+import { ProdutoController } from "./produto.controller";
+import { UsuarioModule } from "src/usuario/usuario.module";
 
 @Module({
-    imports: [],
-    controllers: [],
-    providers: [],
-  })
-  export class ProdutoModule {}
+    imports: [UsuarioModule],
+    controllers: [ProdutoController],
+    //Referencias para Classe que o Nest.js gerencie a criacao desses objetos.
+    providers: [ProdutoRepository],
+})
+export class ProdutoModule { }
