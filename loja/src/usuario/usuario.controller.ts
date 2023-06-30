8bb41a2dc728de862f80240433694cc4fa237119
Robson Coura <robson_coura@yahoo.com.br>
2023-06-30 14:26:23 -0300

    Implementando endpoit atualizarUsuario e atualizarUsuarioDTO

--- loja/src/usuario/usuario.controller.ts
+++ loja/src/usuario/usuario.controller.ts
@@ -1,47 +1,59 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post, Put } from "@nestjs/common";
 import { UsuarioRepository } from "./usuario.repository";
 import { UsuarioEntity } from "./usuario.entity";
 import { v4 as uuid } from 'uuid';
 import { ListaUsuarioDTO } from "./dto/ListaUsuario.dto";
 import { CadastrarUsuarioDTO } from "./dto/CadastrarUsuario.dto";
+import { AtualizarUsuarioDTO } from "./dto/AtualizarUsuario.dto";
 
 
 
 @Controller('/usuarios')
 export class UsuarioController {
 
-    //Construtor para o Nest.js consiga instancia de forma automatica o Objeto
-    constructor(private usuarioRepository: UsuarioRepository) {
+  //Construtor para o Nest.js consiga instancia de forma automatica o Objeto
+  constructor(private usuarioRepository: UsuarioRepository) {
 
+  }
+
+  //Método para cadastro de usuario
+  @Post()
+  async cadastrarUsuario(@Body() dadosDoUsuario: CadastrarUsuarioDTO) {
+
+    const usuarioEntity = new UsuarioEntity();
+    usuarioEntity.id = uuid();
+    usuarioEntity.nome = dadosDoUsuario.nome;
+    usuarioEntity.email = dadosDoUsuario.email;
+    usuarioEntity.senha = dadosDoUsuario.senha;
+
+    this.usuarioRepository.salvar(usuarioEntity);
+
+    return {
+      usuario: new ListaUsuarioDTO(usuarioEntity.id, usuarioEntity.nome),
+      messagem: 'Usuário criado com sucesso',
+    };
+  }
+
+  //Método para consultar todos usuario
+  @Get()
+  async listUsuarios() {
+    const usuariosSalvos = await this.usuarioRepository.listar();
+    const usuariosLista = usuariosSalvos.map(
+      (usuario) => new ListaUsuarioDTO(usuario.id, usuario.nome),
+    );
+
+    return usuariosLista;
+  }
+
+  //Método para atualizar um usuario
+  @Put('/:id')
+  async atualizarusuario(@Param('id') id: string, @Body() dadosParaAtualizar: AtualizarUsuarioDTO) {
+    const usuarioAtualizado = await this.usuarioRepository.atualizar(id, dadosParaAtualizar);
+
+    return {
+      usuario: usuarioAtualizado,
+      messagem: 'Usuário atualizado com sucesso',
     }
+  }
 
-    //Método para cadastro de usuario
-    @Post()
-    async cadastrarUsuario(@Body() dadosDoUsuario: CadastrarUsuarioDTO) {
-
-        const usuarioEntity = new UsuarioEntity();
-        usuarioEntity.id = uuid();
-        usuarioEntity.nome = dadosDoUsuario.nome;
-        usuarioEntity.email = dadosDoUsuario.email;
-        usuarioEntity.senha = dadosDoUsuario.senha;
-
-        this.usuarioRepository.salvar(usuarioEntity);
-
-        return {
-            usuario: new ListaUsuarioDTO(usuarioEntity.id, usuarioEntity.nome),
-            message: 'Usuário criado com sucesso',
-        };
-    }
-
-    //Método para consultar todos usuario
-    @Get()
-    async listUsuarios() {
-      const usuariosSalvos = await this.usuarioRepository.listar();
-      const usuariosLista = usuariosSalvos.map(
-        (usuario) => new ListaUsuarioDTO(usuario.id, usuario.nome),
-      );
-  
-      return usuariosLista;
-    }
-  
 }
--- loja/src/usuario/usuario.repository.ts
+++ loja/src/usuario/usuario.repository.ts
@@ -6,18 +6,18 @@ export class UsuarioRepository {
 
     private usuarios: UsuarioEntity[] = [];
 
-    //Método para salva usuario
+    //Método para salva usuário
     salvar(usuario: UsuarioEntity) {
         this.usuarios.push(usuario);
     }
 
-    //Método para consultar todos usuario
+    //Método para consultar todos usuário
     async listar() {
         return this.usuarios;
     }
 
 
-    //Método de buscar usuario por e-mail
+    //Método de buscar usuário por e-mail
     async existeEsseEmail(email: string) {
         const possivelUsuario = this.usuarios.find(
             usuario => usuario.email === email
@@ -26,4 +26,24 @@ export class UsuarioRepository {
         return possivelUsuario !== undefined;
     }
 
+
+    //Método para atualizar um usuario
+    async atualizar(id: string, dadosDeAtualizacao: Partial<UsuarioEntity>) {
+        const possivelUsuario = this.usuarios.find(
+            usuarioSalvo => usuarioSalvo.id === id
+        );
+        // Verificando se encontrou o usuário, para depois atualizar
+        if (!possivelUsuario) {
+            throw new Error('Usuário não existe');
+        }
+        //Valida se email é realmente um email e verifica se já tem gravado na base de dados
+        Object.entries(dadosDeAtualizacao).forEach(([chave, valor]) => {
+            if (chave === 'id') {
+                return;
+            }
+            possivelUsuario[chave] = valor;
+        });
+
+        return possivelUsuario;
+    }
 }
