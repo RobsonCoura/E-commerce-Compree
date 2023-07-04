548ab93b7ec324f55319867899d08721180cf001
Robson Coura <robson_coura@yahoo.com.br>
2023-07-03 23:21:37 -0300

    Implementando campo id na entity produto-imagem

--- loja/src/produto/produto-imagem.entity.ts
+++ loja/src/produto/produto-imagem.entity.ts
@@ -1,10 +1,14 @@
 import {
     Entity,
-    Column
+    Column,
+    PrimaryGeneratedColumn
 } from "typeorm";
 
 @Entity({ name: 'produto_imagens' })
 export class ProdutoImagem {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
 
     @Column({ name: 'url', length: 100, nullable: false })
     url: string;
