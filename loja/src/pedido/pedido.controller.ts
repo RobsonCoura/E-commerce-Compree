c7a91364b908488dd9e26d23dc5e00d09453abca
Robson Coura <robson_coura@yahoo.com.br>
2023-07-10 21:30:59 -0300

    Criando PedidoController com endpoint criar, buscar e atualizar

---
+++ loja/src/pedido/pedido.controller.ts
@@ -0,0 +1,37 @@
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { AtualizaPedidoDto } from './dto/AtualizaPedido.dto';
+import { CriaPedidoDTO } from './dto/CriaPedido.dto';
+import { PedidoService } from './pedido.service';
+
+/*A camada de controller é responsável por receber as requisições HTTP,
+ e direcioná-las aos serviços adequados*/
+@Controller('pedidos')
+export class PedidoController {
+  constructor(private readonly pedidoService: PedidoService) {}
+  //Endpoint para cadastrar um pedido
+  @Post()
+  async criaPedido(
+    @Query('usuarioId') usuarioId: string,
+    @Body() dadosDoPedido: CriaPedidoDTO,
+  ) {
+    const pedidoCriado = await this.pedidoService.cadastraPedido(
+      usuarioId,
+      dadosDoPedido,
+    );
+    return pedidoCriado;
+  }
+  //Endpoint que retorna uma lista de pedidos relacionados ao usuário especificado pelo usuarioId
+  @Get()
+  async obtemPedidosDeUsuario(@Query('usuarioId') usuarioId: string) {
+    const pedidos = await this.pedidoService.obtemPedidosDeUsuario(usuarioId);
+
+    return pedidos;
+  }
+  //Endpoint para atualizar pedido
+  atualizaPedido(
+    @Param('id') pedidoId: string,
+    @Body() dadosDeAtualizacao: AtualizaPedidoDto,
+  ) {
+    return this.pedidoService.atualizaPedido(pedidoId, dadosDeAtualizacao);
+  }
+}
