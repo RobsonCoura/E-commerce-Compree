cd6a5914aea8e419728fc325cc32857f8a51b92e
Robson Coura <robson_coura@yahoo.com.br>
2023-07-11 13:26:50 -0300

    Refatorando os DTOs, UsuarioDTO e ProdutoDTO

--- loja/src/usuario/dto/AtualizarUsuario.dto.ts
+++ loja/src/usuario/dto/AtualizarUsuario.dto.ts
@@ -1,22 +1,4 @@
-import {
-    IsEmail,
-    IsNotEmpty,
-    IsOptional,
-    MinLength
-} from 'class-validator';
-import { EmailEhUnico } from '../validacao/email-unico.validator';
+import { PartialType } from '@nestjs/mapped-types';
+import { CadastrarUsuarioDTO } from './CadastrarUsuario.dto';
 
-export class AtualizarUsuarioDTO {
-  @IsNotEmpty({ message: 'O nome não pode ser vazio' })
-  @IsOptional()
-  nome: string;
-
-  @IsEmail(undefined, { message: 'O e-mail informado é inválido' })
-  @EmailEhUnico({ message: 'Já existe um usúario com este e-mail' })
-  @IsOptional()
-  email: string;
-
-  @MinLength(6, { message: 'A senha precisa ter pelo menos 6 caracteres' })
-  @IsOptional()
-  senha: string;
-}
+export class AtualizarUsuarioDTO extends PartialType(CadastrarUsuarioDTO) {}
