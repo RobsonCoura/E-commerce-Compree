e0de091f39eb0b4953bd539c7a979a10d8f08829
Robson Coura <robson_coura@yahoo.com.br>
2023-07-11 13:51:10 -0300

    Refatorando código em UsuarioService

--- loja/src/usuario/usuario.service.ts
+++ loja/src/usuario/usuario.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { AtualizarUsuarioDTO } from './dto/AtualizarUsuario.dto';
@@ -12,19 +12,15 @@ export class UsuarioService {
     @InjectRepository(UsuarioEntity)
     private readonly usuarioRepository: Repository<UsuarioEntity>,
   ) {}
-
-  //Método para criar um usuário
+  //Método para cadastrar um usuario
   async criaUsuario(dadosDoUsuario: CadastrarUsuarioDTO) {
     const usuarioEntity = new UsuarioEntity();
 
-    usuarioEntity.email = dadosDoUsuario.email;
-    usuarioEntity.senha = dadosDoUsuario.senha;
-    usuarioEntity.nome = dadosDoUsuario.nome;
+    Object.assign(usuarioEntity, dadosDoUsuario as UsuarioEntity);
 
     return this.usuarioRepository.save(usuarioEntity);
   }
-
-  //Método para buscar uma lista de usuários
+  //Método para trazer uma lista de usuarios
   async listaUsuarios() {
     const usuariosSalvos = await this.usuarioRepository.find();
     const usuariosLista = usuariosSalvos.map(
@@ -32,22 +28,33 @@ export class UsuarioService {
     );
     return usuariosLista;
   }
-
-  //Método para atualizar um usuário
-  async atualizarUsuario(id: string, novosDados: AtualizarUsuarioDTO) {
-    await this.usuarioRepository.update(id, novosDados);
-  }
-
-  //Método para deletar um usuário
-  async deletarUsuario(id: string) {
-    await this.usuarioRepository.delete(id);
-  }
-
-  //Método para verificar antes de cadastrar se já existe esse e-mail na base de dados
+  //Método para verificar se já existe email cadastrado no banco
   async buscaPorEmail(email: string) {
     const checkEmail = await this.usuarioRepository.findOne({
       where: { email },
     });
+
+    if (checkEmail === null)
+      throw new NotFoundException('O email não foi encontrado.');
+
     return checkEmail;
+  }
+  //Método para atualizar um usuario
+  async atualizaUsuario(id: string, novosDados: AtualizarUsuarioDTO) {
+    const usuario = await this.usuarioRepository.findOneBy({ id });
+
+    if (usuario === null)
+      throw new NotFoundException('O usuário não foi encontrado.');
+
+    Object.assign(usuario, novosDados as UsuarioEntity);
+
+    return this.usuarioRepository.save(usuario);
+  }
+  //Método para excluir um usuario
+  async deletaUsuario(id: string) {
+    const resultado = await this.usuarioRepository.delete(id);
+
+    if (!resultado.affected)
+      throw new NotFoundException('O usuário não foi encontrado.');
   }
 }
